883548ab9043a101319993429cb6aac4cd5e4e09
Luiz Carlos B Pereira <carlinhospereira12@hotmail.com>
2022-02-09 00:54:54 -0300

    test commit

--- app/domain/common/ControllerModelInterface.ts
+++ app/domain/common/ControllerModelInterface.ts
@@ -2,4 +2,5 @@ export interface ControllerModelInterface{
   save(model: Record<string, any>): Promise<boolean>
   get(index: string, value: string): Promise<Array<Record<string, any>>>
   delete(index: string, value: string): Promise<boolean>
+  // test 
 }
