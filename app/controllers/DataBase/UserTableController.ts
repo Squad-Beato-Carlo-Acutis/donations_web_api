961c71624625d5db9929a62d788cd55f6069aa77
Luiz Carlos B Pereira <carlinhospereira12@hotmail.com>
2022-03-17 00:37:07 -0300

    separating responsibilities

--- app/controllers/DataBase/UserTableController.ts
+++ app/controllers/DataBase/UserTableController.ts
@@ -1,33 +1,88 @@
+import * as yup from "yup";
+import { TypeTabUsers } from "../../models/TabUsers";
 import { UserTableRepository } from "../../repository/UserTableRepository";
 
 // UserTableController
 export const UserTableController = {
+  fieldValidation: async (
+    user: TypeTabUsers,
+    typeValidation: "create" | "update" = "create"
+  ): Promise<boolean> => {
+    let schema =
+      typeValidation === "create"
+        ? yup.object().shape({
+            email: yup.string().email().required(),
+            pws: yup.string().min(8).required(),
+            username: yup.string().required(),
+            ind_active: yup.boolean().notRequired(),
+          })
+        : yup.object().shape({
+            email: yup.string().email().notRequired(),
+            pws: yup.string().min(8).notRequired(),
+            username: yup.string().notRequired(),
+            ind_active: yup.boolean().notRequired(),
+          });
+
+    let result: any;
+
+    try {
+      result = await schema.validate(user);
+    } catch (error) {
+      result = error;
+    }
+
+    // Validação de campos do formulário
+    if (result.name === "ValidationError") {
+      throw new Error(result.message);
+    }
+
+    return true;
+  },
   getAll: async (req: any, res: any) => {
-    const userController = new UserTableRepository();
-    res.json(await userController.searchAll());
+    const userRepository = new UserTableRepository();
+    res.json(await userRepository.searchAll())
   },
 
   create: async (req: any, res: any) => {
-    const userController = new UserTableRepository();
-    res.json(await userController.create(req.body));
+    try {
+      const userRepository = new UserTableRepository();
+      const userData = req.body;
+      await UserTableController.fieldValidation(userData, "create");
+      const user:any = await userRepository.create(userData)
+      const dadosUser = user.toJSON()
+
+      res.json({
+        id: dadosUser.id,
+        email: dadosUser.email, 
+        responseInfo: {
+          statusCode: 200, 
+          msg: "Usuário cadastrado com sucesso"
+        }
+      })
+    } catch (error) {}
   },
 
   update: async (req: any, res: any) => {
-    const { userId } = req.params
-    const userController = new UserTableRepository();
-    res.json(await userController.update(userId, req.body));
+    const { userId } = req.params;
+    const user = req.body;
+    await UserTableController.fieldValidation(user, "update");
+    const userRepository = new UserTableRepository();
+    res.json(await userRepository.update(userId, user));
   },
 
   getById: async (req: any, res: any) => {
-    const { userId } = req.params
-    const userController = new UserTableRepository();
-    res.json(await userController.searchById(userId));
+    const { userId } = req.params;
+
+    if (!userId) throw new Error("ID do usuário não informado");
+
+    const userRepository = new UserTableRepository();
+    res.json(await userRepository.searchById(userId));
   },
 
   delete: async (req: any, res: any) => {
-    const { userId } = req.params
-    const userController = new UserTableRepository();
-    await userController.delete(userId)
+    const { userId } = req.params;
+    const userRepository = new UserTableRepository();
+    await userRepository.delete(userId);
     res.status(204).json();
-  }
-}
+  },
+};
--- app/repository/UserTableRepository.ts
+++ app/repository/UserTableRepository.ts
@@ -1,45 +1,11 @@
-import * as yup from "yup";
+
 import { encryptSha512 } from "../helpers/encryptSha512";
 import { TabUsers, TypeTabUsers } from "../models/TabUsers";
 
 export class UserTableRepository {
-  async fieldValidation(
-    user: TypeTabUsers,
-    typeValidation: "create" | "update" = "create"
-  ): Promise<boolean> {
-    let schema =
-      typeValidation === "create"
-        ? yup.object().shape({
-            email: yup.string().email().required(),
-            pws: yup.string().min(8).required(),
-            username: yup.string().required(),
-            ind_active: yup.boolean().notRequired(),
-          })
-        : yup.object().shape({
-            email: yup.string().email().notRequired(),
-            pws: yup.string().min(8).notRequired(),
-            username: yup.string().notRequired(),
-            ind_active: yup.boolean().notRequired(),
-          });
-
-    let result: any;
-
-    try {
-      result = await schema.validate(user);
-    } catch (error) {
-      result = error;
-    }
-
-    // Validação de campos do formulário
-    if (result.name === "ValidationError") {
-      throw new Error(result.message);
-    }
-
-    return true;
-  }
+  
 
   async searchById(userId: number): Promise<TabUsers | {}> {
-    if (!userId) throw new Error("ID do usuário não informado");
     const user = await TabUsers.findByPk(userId);
     return user ? user.toJSON() : {};
   }
@@ -49,7 +15,6 @@ export class UserTableRepository {
   }
 
   async create(user: TypeTabUsers): Promise<TabUsers> {
-    await this.fieldValidation(user);
     const encryptedPassword = encryptSha512(user.pws);
 
     const userRegister = await TabUsers.create({
@@ -63,7 +28,7 @@ export class UserTableRepository {
   }
 
   async update(userId: number, data: TypeTabUsers): Promise<TabUsers> {
-    await this.fieldValidation(data, "update");
+    
     if (!userId) throw new Error("ID do usuário não informado");
 
     const user = await TabUsers.findByPk(userId);
