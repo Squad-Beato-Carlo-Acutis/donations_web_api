4627f963a977830817abf1580d31b2abda511d70
Luiz Carlos B Pereira <carlinhospereira12@hotmail.com>
2022-04-12 22:03:59 -0300

    create tables migrations

---
+++ app/database/migrations/20220413003017-create-basic-basket.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('tb_basic_basket', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      tb_user_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        references: { model: 'tb_users', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      ind_active: {
+        type: Sequelize.TINYINT,
+        defaultValue: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('tb_basic_basket');
+  }
+};
