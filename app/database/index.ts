c9956bf8c1a52f014917005e5261b72210b34154
Luiz Carlos B Pereira <carlinhospereira12@hotmail.com>
2022-04-13 21:47:57 -0300

    finished dev of the basic basket route

--- app/database/index.ts
+++ app/database/index.ts
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
+import { TabBasicBasket } from "../models/TabBasicBasket";
 import { TabCategories } from "../models/TabCategories";
 import { TabConfereces } from "../models/TabConfereces";
 import { TabMeasures } from "../models/TabMeasures";
+import { TabProductBasicBasket } from "../models/TabProductBasicBasket";
 import { TabProducts } from "../models/TabProducts";
 import { TabUsers } from "../models/TabUsers";
 const dbConfig = require("../config/database")
@@ -13,7 +15,10 @@ TabConfereces.initialize(conection)
 TabMeasures.initialize(conection)
 TabCategories.initialize(conection)
 TabProducts.initialize(conection)
+TabBasicBasket.initialize(conection)
+TabProductBasicBasket.initialize(conection)
 
 TabUsers.associate(conection.models)
 TabConfereces.associate(conection.models)
 TabProducts.associate(conection.models)
+TabBasicBasket.associate(conection.models)
