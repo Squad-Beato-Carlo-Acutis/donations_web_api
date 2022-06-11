c6c5908b8fec1a4c37339cb0bbf5716db9123160
Luiz Pereira <luiz.pereira@acct.global>
2022-06-11 12:46:53 -0300

    disable create users

--- app/routes.ts
+++ app/routes.ts
@@ -16,7 +16,7 @@ routes.post('/api/v1/login', UserTableController.login)
 
 // Routes User
 routes.get('/api/v1/users', userAuth, UserTableController.getAll)
-routes.post('/api/v1/users', UserTableController.create)
+// routes.post('/api/v1/users', UserTableController.create)
 routes.get('/api/v1/users/:userId', userAuth, UserTableController.getById)
 routes.patch('/api/v1/users/:userId', userAuth, UserTableController.update)
 routes.delete('/api/v1/users/:userId', userAuth, UserTableController.delete)
