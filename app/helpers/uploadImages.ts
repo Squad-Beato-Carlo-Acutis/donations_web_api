4fd43baa02e7e45551e51de5532f760200e3f32b
Luiz Pereira <luiz.pereira@acct.global>
2022-06-26 23:12:30 -0300

    add directory images

--- app/helpers/uploadImages.ts
+++ app/helpers/uploadImages.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images");
+    cb(null, `./${process.env.ENV_IMAGE_DIRECTORY}`);
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now().toString()}-${file.fieldname}`);
--- app/server.ts
+++ app/server.ts
@@ -4,6 +4,7 @@ import { routes } from "./routes";
 
 const app = express();
 
+app.use('/api/v1/images', express.static(`${process.env.ENV_IMAGE_DIRECTORY}`))
 app.use(cors())
 
 app.use(express.json());
